f2ab408674a64f7e0dcbfb04c05e4657bb7441ef
Loukas <loukasbass@gmail.com>
2024-12-09 04:00:42 +0100

    ""

--- next.config.js
+++ next.config.js
@@ -1,34 +1,45 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-const nextConfig = {};
+const nextConfig = {
+  // assetPrefix: './',
+  // trailingSlash: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-if (process.env.APP_ENV === "DEV") {
-  nextConfig = {
-    // assetPrefix: './',
-    // trailingSlash: true,
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-    sassOptions: {
-      includePaths: [path.join(__dirname, "styles")],
-    },
-  };
-} else {
-  nextConfig = {
-    assetPrefix: "./",
-    trailingSlash: true,
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-    sassOptions: {
-      includePaths: [path.join(__dirname, "styles")],
-    },
-  };
-}
+// if (process.env.APP_ENV === "DEV") {
+// const nextConfig = {
+//   // assetPrefix: './',
+//   // trailingSlash: true,
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+//   reactStrictMode: true,
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//   },
+// };
+// } else {
+//   const nextConfig = {
+//     assetPrefix: "./",
+//     trailingSlash: true,
+//     output: "export",
+//     images: {
+//       unoptimized: true,
+//     },
+//     reactStrictMode: true,
+//     sassOptions: {
+//       includePaths: [path.join(__dirname, "styles")],
+//     },
+//   };
+// }
 
 module.exports = nextConfig;
